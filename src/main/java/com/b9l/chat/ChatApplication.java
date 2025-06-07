40c65171fa66eddb59079bf600387f07ccc598d9
KabilBoufares <boufareskabil209@gmail.com>
2025-06-07 17:27:57 +0100

    initial commit

---
+++ src/main/java/com/b9l/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.b9l.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
